a48cdc8c4db6e43d2f365f5d900706c5bc940198
Leandro <142351965+Leandro-Madrid@users.noreply.github.com>
2024-09-30 13:27:20 -0300

    Update Main.java

--- src/main/java/ar/edu/davinci/Main.java
+++ src/main/java/ar/edu/davinci/Main.java
@@ -1,7 +1,5 @@
 package ar.edu.davinci;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         CajaAhorro CajaAhorro = new CajaAhorro("CA1");
